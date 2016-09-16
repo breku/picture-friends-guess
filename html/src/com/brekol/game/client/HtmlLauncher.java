e57be9f9c49d2e86b49e932dd11f27737f1b8f5c
brekol <brekol@gmail.com>
2016-09-16 19:38:44 +0200

    init commit

---
+++ html/src/com/brekol/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.brekol.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.brekol.game.PirctureFriendsGuessApp;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new PirctureFriendsGuessApp();
+        }
+}
